45d8f1da7b4a12ca3d61a08dabf862afb9bf842e
Shawty97 <81822092+Shawty97@users.noreply.github.com>
2025-01-11 23:39:51 +0100

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -32,11 +32,7 @@ export default async function Account() {
     <section className="mb-32 bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
         <div className="sm:align-center sm:flex sm:flex-col">
-      <Pricing
-      user={user}
-      products={products ?? []}
-      subscription={subscription}
-    />
+
           <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
           </p>
         </div>
